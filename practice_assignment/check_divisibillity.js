fd03258a390f7ca9ceec40ca926949ba652f701d
Raval Vandan <120940947+vandan501@users.noreply.github.com>
2023-03-09 18:42:58 +0530

    Update check_divisibillity.js

--- practice_assignment/check_divisibillity.js
+++ practice_assignment/check_divisibillity.js
@@ -1,13 +1,15 @@
 /*9. Check for divisibilityP
 Writq a pro[ram that takqs an array of numbqrs and prints all thq numbqrs that arq divisiblq by 3, but not by 2.
 Usq a for loop and continuq statqmqnt.*/
-const numbers = [90];
+const numbers = [1,2,3,4,5,6,7,8,9,10];
 let count = 0;
 
-for (let i = 0; i < numbers; i++) {
-  if (numbers[i] % 3 === 0 && numbers[i] % 2 !== 0) {
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 3 === 0 && numbers[i] % 2 !== 0)
+   {
     count++;
   }
 }
 
 console.log(count); 
+ 
