918c17fc7fe30725d4e138d6b32f10bbef4144b5
Raval Vandan <120940947+vandan501@users.noreply.github.com>
2023-04-02 00:14:22 +0530

    Add files via upload

---
+++ Error_Handling_and_prototype/car_description_class.js
@@ -0,0 +1,18 @@
+/*Create a class called Car with three properties: company, model, and year. The class should have a method called getDescription that returns a string in the format "This is a <year> <company> <model>". Instantiate an instance of the Car class and call the getDescription method.*/
+
+class Car {
+    constructor(company, model, year) {
+      this.company = company;
+      this.model = model;
+      this.year = year;
+    }
+    
+    getDescription() {
+      return `This is a ${this.year} ${this.company} ${this.model}.`;
+    }
+  }
+  
+  let car = new Car("Honda", "Jazz", 2021);
+  console.log(car.getDescription()); // Output: This is a 2021 Honda Jazz.
+  console.log(car); // Output: Car { company: 'Honda', model: 'Jazz', year: 2021 }
+  
